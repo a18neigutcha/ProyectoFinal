414c1f9c58a890d504cb81f80183fcfeff203c3e
Ubuntu <a18neigutcha@inspedralbes.cat>
2020-05-19 00:07:26 +0200

    Merge realizado

--- App/src/components/MenuLogIn.vue
+++ App/src/components/MenuLogIn.vue
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-=======
-import SignIn from "../views/SignIn.vue";
-import SignUp from "../views/SignUp.vue";
-import UserPage from "../views/UserPage.vue";
-<style>
-     .navbar {
-         border: none;
-         box-shadow: 0 1px 20px 0 rgba(0,0,0,.1);
-         width: 400%;
-         height: 100%;
-    }
-
-</style>
-
-
->>>>>>> 0364cc77e562b55c7488b5a318a9722151242ac8
 <template>
   <div class="navbar navbar-expand-lg navbar-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,19 +9,6 @@ import UserPage from "../views/UserPage.vue";
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
             <ul class="navbar-nav ">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                  
-=======
-=======
->>>>>>> 0364cc77e562b55c7488b5a318a9722151242ac8
-                <li class="nav-item ml-3">
-                  <form class="form-inline" action="/action_page.php">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Encuentra lugares  ">
-                        <button class="btn btn-success" type="submit">Búsqueda</button>
-                    </form>
-                </li>   
->>>>>>> 0364cc77e562b55c7488b5a318a9722151242ac8
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
                         ENTRA
