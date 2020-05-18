f599eb1889e8f2ad5ffafc19f14169ce97dab5a5
Alexandros <a18aleestseg@inspedralbes.cat>
2020-05-18 22:59:29 +0200

    Reestructuracion logica barra menus e inicio de la logica de login,registro, auntentificacion

--- App/src/views/SignIn.vue
+++ App/src/views/SignIn.vue
@@ -1,13 +1,59 @@
 <template>
     <div class="SignIn">
-
+        <div class="row">
+            <div class="col">
+                <div class="card-deck">
+                    <CartaLugar
+                        v-for="(lugar,id) in datos" :key="id"
+                        :titulo="lugar.nombre"
+                        :subtitulo="lugar.direccion"
+                        :descripcion="lugar.descripcion"
+                        :dirUrl="lugar.dirUrl"
+                    ></CartaLugar>   
+                </div>
+            </div>
+                        
+        </div>
     </div>
 </template>
 <script>
 
+import axios from 'axios';
+
 export default {
     name: "SignIn",
-    components: {
-    }
+  components:{
+      CartaLugar
+  },
+  data:function() {
+      return {
+          datos:{
+              titulo:"",
+              subtitulo:"",
+              descripcion:"",
+              url:""
+
+          },
+          axx:0
+      }
+      
+
+  },
+  mounted () {
+    axios.get('http://localhost:3000/api/',{
+        headers: {
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+        }
+    })
+    .then(response =>{
+        this.datos=response.data;
+        console.log(response.data);
+    })
+  }
 }
 </script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
