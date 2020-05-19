959e11454860a58630cd4833fa7d57c307a45578
Alexandros <a18aleestseg@inspedralbes.cat>
2020-05-19 22:45:54 +0200

    Registro de usuario creado

--- App/src/views/SignUp.vue
+++ App/src/views/SignUp.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-container>
+  <form id="SignUp">
+        <h1>Registro de Usuario</h1>
+        <input type="text" name="username" v-model="input.username" placeholder="Nombre Usuario" />
+        <input type="text" name="email" v-model="input.email" placeholder="Email Usuario" />
+        <input type="password" name="password" v-model="input.email" placeholder="Contraseña" />
+        <input type="password" name="confirmaPassword" v-model="input.password" placeholder="Confirma Contraseña" />
+        <button type="button" v-on:click="CreateUser()">Regístrate</button>
+    </form> 
+</template>
+
+<!--   <v-container>
       <v-layout row>
           <v-flex xs12 sm6 m4 offset-xs0 offset-sm3 offset-m4>
               <v-form class=formulario>
@@ -8,12 +18,11 @@
                   <v-text-field label="Contraseña" type="password"></v-text-field>
                   <v-text-field label="Confirmar Contraseña" type="password"></v-text-field>
                   <v-btn block color="button" v-on:click="submit()">Regístrate! </v-btn>
-                  <!-- <v-btn block color="primary" @click="$router.push('/principal')">Registro de Usuario</v-btn> -->
-              </v-form>
+                <v-btn block color="primary" @click="$router.push('/principal')">Registro de Usuario</v-btn>
+            </v-form>
           </v-flex>
       </v-layout>
-  </v-container>
-</template>
+  </v-container> -->
 
 
 <script>
@@ -42,13 +51,17 @@
                     console.log(response.data);
 
                     //Redirige al inicio
-                    this.$route.push("/");
+                    this.$route.push("@/Home");
 
                 },(error) => {
                     console.log(error.response.data);
                     alert("Usuario no registrado!")
                 });
-            }
+        },
+        function(){
+
+        },
+        
     }
             
 }
