5ed4a453bf126387a6977838473245895552b358
Ubuntu <a18neigutcha@inspedralbes.cat>
2020-05-19 23:35:21 +0200

    Merge finalizado

--- App/src/components/MenuLogIn.vue
+++ App/src/components/MenuLogIn.vue
@@ -4,32 +4,24 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
             <ul class="navbar-nav ">
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="http://localhost:3000">Api</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <router-link class="nav-link" to="/Ayuda">Help</router-link>
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                        ENTRA
+                        <i v- class="fas fa-sign-in-alt"></i>
+                         Entra
+                        
                     </a>
                     <div class="dropdown-menu">
                         <router-link class="dropdown-item" to="/SignIn">Inicia sesión</router-link>
                         <router-link class="dropdown-item" to="/SignUp">Registrate</router-link>
                         <router-link class="dropdown-item" to="/UserPage">Panel Usuario</router-link>
                     </div>
-                </li>
-                <li class="nav-item ml-3">
-                    
-                    <router-link class="nav-link" to="/Sesion">
-                        <i class="fas fa-sign-in-alt"></i>
-                    </router-link>
-                </li>
-                <li class="nav-item mx-3">
-                    <a class="nav-link" href="http://localhost:3000">Api</a>
-                </li>
-                <li class="nav-item mx-3">
-                    <router-link class="nav-link" to="/Ayuda">Help</router-link>
                 </li>
             </ul>
         </div>
