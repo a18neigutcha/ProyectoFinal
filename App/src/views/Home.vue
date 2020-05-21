83486bff5d1978ed81f5f84a819ba8e730f10ce2
Alexandros <a18aleestseg@inspedralbes.cat>
2020-05-22 00:01:29 +0200

    Diseño landing pre carrousel optimizado

--- App/src/views/Home.vue
+++ App/src/views/Home.vue
@@ -21,6 +21,24 @@
           </div>
         </div>
 
+        <div class="card-section">
+        <div class="container">
+            <div class="card-block bg-white mb30">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <!-- section-title -->
+                        <div class="section-title mb-0">
+                            <h2>Lugares destacados</h2>
+                            <p>Our approach is very simple: we define your problem and give the best solution. </p>
+                        </div>
+                        <!-- /.section-title -->
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
 
         <div class="row mt-3">
 
@@ -55,4 +73,27 @@ export default {
   .imagenPortada{
     height: 30em;
   }
+  body { -webkit-font-smoothing: antialiased; text-rendering: optimizeLegibility; font-family: 'Overpass', sans-serif; letter-spacing: 0px; font-size: 17px; color: #8d8f90; font-weight: 400; line-height: 32px; background-color: #edefef; }
+  h1, h2, h3, h4, h5, h6 { color: #25292a; margin: 0px 0px 10px 0px; font-family: 'Overpass', sans-serif; font-weight: 700; letter-spacing: -1px; line-height: 1; }
+  h1 { font-size: 34px; }
+  h2 { font-size: 28px; line-height: 38px; }
+  h3 { font-size: 22px; line-height: 32px; }
+  h4 { font-size: 20px; }
+  h5 { font-size: 17px; }
+  h6 { font-size: 12px; }
+  p { margin: 0 0 20px; line-height: 1.7; }
+  p:last-child { margin: 0px; }
+  ul, ol { }
+  a { text-decoration: none; color: #8d8f90; -webkit-transition: all 0.3s; -moz-transition: all 0.3s; transition: all 0.3s; }
+  a:focus, a:hover { text-decoration: none; color: #f85759; }
+
+
+
+  .page-header { background: url(https://easetemplate.com/free-website-templates/hike/images/pageheader.jpg)no-repeat; position: relative; background-size: cover; }
+  .page-caption { padding-top: 170px; padding-bottom: 174px; }
+  .page-title { font-size: 46px; line-height: 1; color: #fff; font-weight: 600; text-align: center; }
+
+  .card-section { position: relative; bottom: 60px; }
+  .card-block { padding: 80px; }
+  .section-title { margin-bottom: 60px; }
 </style>
