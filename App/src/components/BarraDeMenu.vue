48b592524ee0f2f2a89907c93927904252327746
Alexandros <a18aleestseg@inspedralbes.cat>
2020-05-25 19:35:22 +0200

    Signin diseñado

--- App/src/components/BarraDeMenu.vue
+++ App/src/components/BarraDeMenu.vue
@@ -1,91 +1,67 @@
 <template>
 
-<div class="header-wrapper">
-        <div class="container">
-            <header role="banner">
-                 <div class="row row-with-vspace site-branding">
-					<div class="col-sm-2  site-title">
-						<h1 class="site-title-heading">
-                         <router-link class="navbar-brand mb-0 h1" to="/">
+<div class="container-fluid ">
+        <!-- Barra de la cabecera -->
+        <div class="row">
+            <div class="col">
+                <div class="navbar navbar-expand-lg navbar-light ">
+                    <!-- Logo de la pagina web -->
+                    <router-link class="navbar-brand mb-0 h1" to="/">
                         <img src="@/assets/logo.png" class="logoImg" alt="Logo de Barcelona desconocida">
                         {{nombreApp}}
                     </router-link>
-                                
-                        </h1>
-						
-					</div><!-- Close col sm2-->
-					<div class="col-sm-10 page-header-top-right">
-                    
-                    <div class="site-description">
-                    <div class="row">
-							<div class="col-sm-8">
-								<h1>This Is The Site Description</h1><br>
-					</div> <!-- Close col sm8-->
-                                <div class="col-sm-4">  
-						<div class="pull-right">
-							<div id="simple-social-icons-3" class="widget simple-social-icons"><ul class="alignleft"><li class="social-facebook"><a href="https://www.facebook.com/" target="_blank">&#xe606;</a></li><li class="social-instagram"><a href="https://www.instagram.com/" target="_blank">&#xe600;</a></li><li class="social-twitter"><a href="https://www.instagram.com/" target="_blank">&#xe607;</a></li><li class="social-youtube"><a href="https://www.youtube.com/" target="_blank">&#xe60f;</a></li></ul></div> 
-						</div>
-						<div class="clearfix"></div>
-						 
-							</div><!-- Close col sm4-->
-						</div><!-- Close col row-->
-                        
-                        </div><!-- Close col site description-->
-                        
-                        
-                   <div class="row main-navigation">
-					<div class="col-md-12">
-                   
-						<nav class="navbar navbar-default" role="navigation">
-							<div class="navbar-header">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-primary-collapse">
-									<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-							</div>
-							
-							<div class="collapse navbar-collapse navbar-primary-collapse">
-								<ul id="menu-main-menu" class="nav navbar-nav"><li id="menu-item-28" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-28"><a href="#">Home</a></li>
-                                <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-29 dropdown" data-dropdown="dropdown"><a href="#about-us/" class="dropdown-toggle" data-toggle="dropdown">About <span class="caret"></span> </a>
-                                <ul class="sub-menu dropdown-menu">
-                                    <li id="menu-item-62" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-62"><a href="#about-us/faqs/">FAQs</a></li>
-                                    <li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-30"><a href="#about-us/mission/">Mission</a></li>
-                                    <li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><a href="#about-us/company/">Company</a></li>
-                                </ul>
-                                </li>
-                                <li id="menu-item-35" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-15 current_page_item menu-item-35 active active"><a href="#locations/">Locations</a></li>
-                                <li id="menu-item-37" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-37"><a href="#our-menu/">Menu</a></li>
-                                <li id="menu-item-40" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-40"><a href="#category/events/">Events</a></li>
 
-                                <li id="menu-item-36" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-36"><a href="#news/">News</a></li>
-
-                                <li id="menu-item-32" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-32"><a href="#contact-f27/">Contact</a></li>
-                                </ul> 
-                               
-							</div><!--.navbar-collapse-->
-						</nav>
-					</div>
-				</div><!--.main-navigation-->
-						<div class="sr-only">
-							<a href="#content" title="Skip to content">Skip to content</a>
-						</div>
-						
-					</div>
-				</div><!--row .site-branding-->
-				
-				
-			</header>
-        </div><!--.container-->
-         <!-- Barra de subMenu -->
- <!--        <div class="row">
+                    <!-- Button despliega menu en pagina redimensionada -->
+                    <button class="navbar-toggler justify-content-end" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
+                        <ul class="navbar-nav ">
+                            <!-- Item Api -->
+                            <li class="nav-item mx-3">
+                                <a class="nav-link" href="http://localhost:3000">Api</a>
+                            </li>
+                            <!-- Item Help -->
+                            <li class="nav-item mx-3">
+                                <router-link class="nav-link" to="/Ayuda">Help</router-link>
+                            </li>
+                            <!-- Item Registrarte -->
+                            <li v-show="!sesionInit" class="nav-item mx-3">
+                                <router-link class="nav-link" to="/SignUp">Registrate</router-link>
+                            </li>
+                            <!-- Item LogIn (Se muestrs si el no hay una session iniciada) -->
+                            <li v-show="!sesionInit" class="nav-item mx-3">
+                                <router-link class="nav-link" to="/SignIn">
+                                    <i v- class="fas fa-sign-in-alt"></i>
+                                    Inicia sesión
+                                    
+                                </router-link>
+                            </li>
+                            <!-- Item User (Solo se muestra si el usuario inicio sesión) -->
+                            <li v-show="sesionInit" class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                    <i v- class="fas fa-user"></i>
+                                </a>
+                                <!-- Opciones de usuario. -->
+                                <div class="dropdown-menu">
+                                    <router-link class="dropdown-item" to="/UserPage">Tu perfil</router-link>
+                                    <router-link class="dropdown-item" to="/Configuracion">Configuración</router-link>
+                                    <button class="dropdown-item" @click="logOut()">Cerrar sesión</button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <!-- Barra de subMenu -->
+        <div class="row">
             <div class="col">
                 <SubMenu :sesionInit="$cookies.get('token')"/>
             </div>
             
-        </div> -->
-    </div><!--.header-wrapper-->
+        </div>
+    </div>
 
     
    
@@ -95,7 +71,7 @@
 
 <script>
 
-/*     import SubMenu from "./SubMenu.vue";
+    import SubMenu from "./SubMenu.vue";
 
     export default {
         name: 'BarraDeMenu',
@@ -113,15 +89,15 @@
                 this.sesionInit = this.$cookies.get("token");
             }
         }
-    } */
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/*     .logoImg{
+    .logoImg{
         width: 3em;
         height: 3em;
-    } */
+    }
     .header-wrapper{
 
     background: #f5f5f5; 
@@ -228,7 +204,7 @@
         font-style: normal;
         font-weight: normal;
         src: url("@/font/ssi-icomoon.eot?#iefixssi-texv9a") format("embedded-opentype"), url("../font/ssi-icomoon.woff?ssi-texv9a") format("woff"), url("../font/ssi-icomoon.ttf?ssi-texv9a") format("truetype"), url("../font/ssi-icomoon.svg?ssi-texv9a#ssi-icomoon") format("svg");
-    } */
+ } */
     .simple-social-icons {
         overflow: hidden;
     }
--- App/src/views/SignIn.vue
+++ App/src/views/SignIn.vue
@@ -1,10 +1,59 @@
 <template>
-    <div id="SignIn">
+<div class="fondo">
+    <div id="SignIn" class="container">
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Inicio Sesión</h3>
+                    <div class="d-flex justify-content-end social_icon">
+                        <span><i class="fab fa-facebook-square"></i></span>
+                        <span><i class="fab fa-google-plus-square"></i></span>
+                        <span><i class="fab fa-twitter-square"></i></span>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" name="username" class="form-control"  v-model="input.email" placeholder="Email Usuario">
+                            
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" class="form-control" v-model="input.password" placeholder="Contraseña">
+                
+                        </div>
+                        <div class="row align-items-center remember">
+                            <input type="checkbox">Recuérdame
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn float-right login_btn" v-on:click="submit()">Entra</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-center links">
+                        No tienes cuenta?<a href="#">Regístrate</a>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <a href="#">¿Olvidaste la contraseña?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--     <div id="SignIn">
         <h1>Login</h1>
         <input type="text" name="username" v-model="input.email" placeholder="Email Usuario" />
         <input type="password" name="password" v-model="input.password" placeholder="Contraseña" />
         <button type="button" v-on:click="submit()">Inicia Sesión</button>
-    </div> 
+    </div>  -->
 </template>
 
 
@@ -57,10 +106,98 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    #secure {
+/*     #secure {
         background-color: #FFFFFF;
         border: 1px solid #CCCCCC;
         padding: 20px;
         margin-top: 10px;
-    }
+} */
+
+@import url('https://fonts.googleapis.com/css?family=Numans');
+
+.fondo{
+background-image: url('http://getwallpapers.com/wallpaper/full/a/2/3/1087941-barcelona-city-wallpapers-1920x1080-high-resolution.jpg');
+background-size: cover;
+background-repeat: no-repeat;
+height: 700px;
+font-family: 'Numans', sans-serif;
+}
+
+.container{
+height: 80%;
+align-content: center;
+}
+
+.card{
+height: 370px;
+margin-top: auto;
+margin-bottom: auto;
+width: 400px;
+background-color: rgba(0,0,0,0.5) !important;
+}
+
+.social_icon span{
+font-size: 60px;
+margin-left: 10px;
+color: #FFC312;
+}
+
+.social_icon span:hover{
+color: white;
+cursor: pointer;
+}
+
+.card-header h3{
+color: white;
+}
+
+.social_icon{
+position: absolute;
+right: 20px;
+top: -45px;
+}
+
+.input-group-prepend span{
+width: 50px;
+background-color: #FFC312;
+color: black;
+border:0 !important;
+}
+
+input:focus{
+outline: 0 0 0 0  !important;
+box-shadow: 0 0 0 0 !important;
+
+}
+
+.remember{
+color: white;
+}
+
+.remember input
+{
+width: 20px;
+height: 20px;
+margin-left: 15px;
+margin-right: 5px;
+}
+
+.login_btn{
+color: black;
+background-color: #FFC312;
+width: 100px;
+}
+
+.login_btn:hover{
+color: black;
+background-color: white;
+}
+
+.links{
+color: white;
+}
+
+.links a{
+margin-left: 4px;
+}
 </style>
